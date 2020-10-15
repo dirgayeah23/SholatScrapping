72f7acc0e213310e95e25c7b21fc5a0f04fbc74f
dirgayeah23 <rezadirgantara91@gmail.com>
2020-10-15 12:39:17 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import bs4 
+import requests
+
+url = "https://jadwalsholat.org/adzan/monthly.php?id=215"
+content = requests.get(url)
+
+response = bs4.BeautifulSoup(content.text,"html.parser")
+data = response.find_all('tr', 'table_highlight')
+data = data[0]
+# print(data)
+
+sholat = {}
+i = 0
+for d in data :
+    if i == 1 :
+        sholat['Imsyak'] = d.get_text()
+    elif i == 2 :
+        sholat['Subuh'] = d.get_text()    
+    elif i == 3 :
+        sholat['Terbit'] = d.get_text()    
+    elif i == 4 :
+        sholat ['Dhuha'] = d.get_text()
+    elif i == 5 :
+        sholat ['Dzuhur'] = d.get_text()          
+    elif i == 6 :
+        sholat ['Azhar'] = d.get_text()
+    elif i == 7 :
+        sholat ['Maghrib'] = d.get_text()        
+    elif i == 8 :
+        sholat ['Isya'] = d.get_text()
+    i+=1    
+
+print('------------------------------')
+print('Waktu Sholat (Raha) Hari ini ')
+print('-----------------------------')
+print('\n'.join("{}: {}".format(k, v) for k, v in sholat.items()))
+
+
